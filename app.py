ffe29708f1a83e50e390f0d2e365ced4e24fc41c
DavidLynch26 <lynchda78@gmail.com>
2023-04-21 21:24:33 -0500

    Added html pages for login and base template/index page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 import mysql.connector
+from forms import LoginForm
 from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 host = "localhost"
 username = "COMP3161Group"
@@ -19,7 +24,7 @@ def connectToDB():
 
     return conn, cursor
 
-@app.route('/Courses', methods = ['GET']) #templte app.route
+@app.route('/Courses', methods = ['GET'])
 def Courses():
     try:
         query = "SELECT * FROM Courses"
@@ -74,49 +79,52 @@ def courseByLecturer(lecturer_id):
         return str(e)
         print(str(e))
 
-@app.route('/Login/<user_id>&<user_password>', methods = ['POST'])
-def login(user_id, user_password):
-    try:
-        if(user_id[0] == "S"):
-            query = f"SELECT * FROM Students WHERE `Student ID` = {user_id!r} AND Password = {user_password!r}"
-        else:
-            query = f"SELECT * FROM Lecturers WHERE `Lecturer ID` = {user_id!r} AND Password = {user_password!r}"
-        conn, cursor = connectToDB()
-        loginStatus = ""
-        cursor.execute(query)
-        row = cursor.fetchone()
-        if row is not None:
-            if(user_id[0] == "S"):
-                studentID, firstName, lastName, email, age, birthday, password = row
-                student = {}
-                student['Student ID'] = studentID
-                student['First Name'] = firstName
-                student['Last Name'] = lastName
-                student['Email'] = email
-                student['Age'] = age
-                student['Birthday'] = birthday
-                student['Password'] = password
-                loginStatus = "Student login successful"
-            else:
-                lecturerID, firstName, lastName, email, age, birthday, password = row
-                lecturer = {}
-                lecturer['Lecturer ID'] = lecturerID
-                lecturer['First Name'] = firstName
-                lecturer['Last Name'] = lastName
-                lecturer['Email'] = email
-                lecturer['Age'] = age
-                lecturer['Birthday'] = birthday
-                lecturer['Password'] = password
-                loginStatus = "Lecturer login successful"
-            return make_response(loginStatus, 200)
-        else:
-            return make_response("User not found", 200)
-    except Exception as e:
-        return make_response(str(e), 400)
+# @app.route('/Login/<user_id>&<user_password>', methods = ['POST'])
+# def login(user_id, user_password):
+#     try:
+#         if(user_id[0] == "S"):
+#             query = f"SELECT * FROM Students WHERE `Student ID` = {user_id!r} AND Password = {user_password!r}"
+#         else:
+#             query = f"SELECT * FROM Lecturers WHERE `Lecturer ID` = {user_id!r} AND Password = {user_password!r}"
+#         conn, cursor = connectToDB()
+#         loginStatus = ""
+#         cursor.execute(query)
+#         row = cursor.fetchone()
+#         if row is not None:
+#             if(user_id[0] == "S"):
+#                 studentID, firstName, lastName, email, age, birthday, password = row
+#                 student = {}
+#                 student['Student ID'] = studentID
+#                 student['First Name'] = firstName
+#                 student['Last Name'] = lastName
+#                 student['Email'] = email
+#                 student['Age'] = age
+#                 student['Birthday'] = birthday
+#                 student['Password'] = password
+#                 loginStatus = "Student login successful"
+#             else:
+#                 lecturerID, firstName, lastName, email, age, birthday, password = row
+#                 lecturer = {}
+#                 lecturer['Lecturer ID'] = lecturerID
+#                 lecturer['First Name'] = firstName
+#                 lecturer['Last Name'] = lastName
+#                 lecturer['Email'] = email
+#                 lecturer['Age'] = age
+#                 lecturer['Birthday'] = birthday
+#                 lecturer['Password'] = password
+#                 loginStatus = "Lecturer login successful"
+#             return make_response(loginStatus, 200)
+#         else:
+#             return make_response("User not found", 200)
+#     except Exception as e:
+#         return make_response(str(e), 400)
 
-@app.route('/', methods = ['GET'])
-def home():
-    return render_template("home.html")
+@app.route('/login', methods = ['GET'])
+def login():
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     return "asd"
+    return render_template("login.html", form = form)
 
 if __name__ == 'main':
     app.run()
