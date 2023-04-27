bfc0189cdc9b0b84bc87f116b515a689f28227ce
DavidLynch26 <lynchda78@gmail.com>
2023-04-27 11:59:42 -0500

    Added classes for additional information

--- forms.py
+++ forms.py
@@ -5,3 +5,16 @@ from wtforms.validators import InputRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class eventForm(FlaskForm):
+    eventName = StringField('Event Name', validators=[InputRequired()])
+    eventType = StringField('Event Type', validators=[InputRequired()])
+    eventDesciption = StringField('Username', validators=[InputRequired()])
+    eventDate = DateField('Event Date', format = '%d/%m/%Y', validators=[InputRequired()])
+
+class assignmentForm(FlaskForm):
+    assignmentName = StringField('Assignment Name', validators=[InputRequired()])
+    assignmentType = StringField('Assignment Type', validators=[InputRequired()])
+    assignmentDesciption = StringField('Assignment Description', validators=[InputRequired()])
+    assignmentStartDate = DateField('Start Date', format = '%d/%m/%Y', validators=[InputRequired()])
+    assignmentEndDate = DateField('End Date', format = '%d/%m/%Y', validators=[InputRequired()])
